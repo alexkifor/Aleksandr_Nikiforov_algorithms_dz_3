d5f2074723da15d409a603bba21f596b83f122e3
Александр Никифоров <104163155+alexkifor@users.noreply.github.com>
2022-10-12 21:39:10 +0300

    Update task_3_3.py

--- task_3_3.py
+++ task_3_3.py
@@ -2,9 +2,9 @@ s = input('Введите строку: ')
 set_res = set()
 dict_res = {}
 for i in range(len(s)):
-        for j in range(i + 1, len(s) if i == 0 else len(s) + 1):
-            set_res.add(hash(s[i:j]))
-            dict_res[s[i:j]] = hash(s[i:j])
+    for j in range(i + 1, len(s) if i == 0 else len(s) + 1):
+        set_res.add(hash(s[i:j]))
+        dict_res[s[i:j]] = hash(s[i:j])
 
 print(f'Количество подстрок в строке {s} равно {len(set_res)}')
 print(list(dict_res.keys()))
