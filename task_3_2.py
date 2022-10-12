85443dd367f8839c00a4b9bb72674b78f69ea25a
Александр Никифоров <104163155+alexkifor@users.noreply.github.com>
2022-10-12 21:51:42 +0300

    Update task_3_2.py

--- task_3_2.py
+++ task_3_2.py
@@ -1,15 +1,17 @@
 import hashlib
 from uuid import uuid4
-password = input('введите пароль из латинских букв и цифр: ')
 salt = uuid4().hex
-res = hashlib.sha256(salt.encode() + password.encode()).hexdigest()
+def create_hash():
+    password = input('введите пароль из латинских букв и цифр: ')
+    res = hashlib.sha256(salt.encode() + password.encode()).hexdigest()
+    return res
+res = create_hash()   
 file = open('passwd.csv', 'w')
 file.write(res)
 file.close()
 while True:
     try:
-        password_1 = input('введите пароль из латинских букв и цифр: ')
-        res_1 = hashlib.sha256(salt.encode() + password_1.encode()).hexdigest()
+        res_1 = create_hash()
     except TypeError:
         print('ошибка при вводе пароля')
     else:
