b2d3847fc159a0e623a6346ceff5fdaa7de12d52
Александр Никифоров <104163155+alexkifor@users.noreply.github.com>
2022-10-12 21:40:27 +0300

    Update task_3_4.py

--- task_3_4.py
+++ task_3_4.py
@@ -1,15 +1,15 @@
 import hashlib
-kash = {'ya.ru': 'afcb4329d72e4e1c4ff20792c11c5221f29089ce212748782ef2e84f845ca027'}
+сash = {'ya.ru': 'afcb4329d72e4e1c4ff20792c11c5221f29089ce212748782ef2e84f845ca027'}
 salt = 'my_salt'
 def check_url(url):
-    if url in kash.keys():
-        return f'хэш {url} - {kash[url]}'
+    if url in сash.keys():
+        return f'хэш {url} - {сash[url]}'
     else:
         hash_url = hashlib.sha256(salt.encode() + url.encode()).hexdigest()
-        kash.setdefault(url, hash_url)
+        сash.setdefault(url, hash_url)
         return f'{url} добавлен в кэш'
 
 
 print(check_url('ya.ru'))
 print(check_url('gb.ru'))
-print(kash)
+print(сash)
